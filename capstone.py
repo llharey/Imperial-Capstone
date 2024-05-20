16c8091f06f38d405a137032a78ddf802c138176
llharey <38376909+llharey@users.noreply.github.com>
2024-05-20 22:11:29 +0100

    Create capstone.py

---
+++ capstone.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+!pip install streamlit_jupyter
+
+x = np.linspace(0, 6000, 1000) # generate 1000 equaly spaced values between 0 and 6000 
+y = np.linspace(0, 4000, 1000 ) # generate 1000 equaly spaced values between 0 and 4000
+
+# determine the plot size
+plt.figure(figsize= (15, 10))
+# Plot the constraint equation
+plt.plot(x, 140*(40 - x/200), label="x/200 + y/140 = 40 ---> C1")
+# Plot the constraint lines
+plt.axvline(x=6000, color='k', linestyle='--', label="x = 6000")
+plt.axhline(y=4000, color='brown', linestyle='--', label="y = 4000")
+# # limits
+plt.title('Graph of Constraints')
+plt.xlabel('Type X', fontsize = 20)
+plt.ylabel('Type Y', fontsize = 20)
+# # legend
+plt.legend(bbox_to_anchor=(1, 1), loc=1, borderaxespad=0.)
+
+plt.show();
